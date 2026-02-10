67c96e4ceffa8e7929cb71adf9b9a1fffdbe02ef
JackieChen <68220954+cc9611@users.noreply.github.com>
2026-02-10 18:18:15 +0800

    Add .gitignore for Flutter project artifacts

---
+++ lib/models/user_profile.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_profile.g.dart';
+
+@JsonSerializable()
+class UserProfile {
+  const UserProfile({
+    required this.id,
+    required this.name,
+  });
+
+  final String id;
+  final String name;
+
+  factory UserProfile.fromJson(Map<String, dynamic> json) =>
+      _$UserProfileFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserProfileToJson(this);
+}
